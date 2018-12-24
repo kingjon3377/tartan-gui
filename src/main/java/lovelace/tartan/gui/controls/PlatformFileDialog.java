kingjon3377/tartan-gui

commit 03a0a9238f7272f26d8650b4a12b1ae93a012fc9

PlatformFileDialog: Declare wrapped component as Component, not Object.

src/main/java/lovelace/tartan/gui/controls/PlatformFileDialog.java
@@ -1,5 +1,6 @@
 package lovelace.tartan.gui.controls;
 
+import java.awt.Component;
 import java.awt.FileDialog;
 import java.awt.Frame;
 import java.io.File;
@@ -21,7 +22,7 @@ public final class PlatformFileDialog {
 	 * The wrapped dialog. Must be either a {@link java.awt.FileDialog} or {@link
 	 * javax.swing.JFileChooser}.
 	 */
-	private final @NotNull Object wrapped;
+	private final @NotNull Component wrapped;
 	/**
 	 * The parent window for this dialog.
 	 */
@@ -113,7 +114,7 @@ public final class PlatformFileDialog {
 	public void showOpenDialog() {
 		if (wrapped instanceof FileDialog) {
 			((FileDialog) wrapped).setMode(FileDialog.LOAD);
-			((FileDialog) wrapped).setVisible(true);
+			wrapped.setVisible(true);
 		} else {
 			((JFileChooser) wrapped).showOpenDialog(parent);
 		}
@@ -124,7 +125,7 @@ public final class PlatformFileDialog {
 	public void showSaveAsDialog() {
 		if (wrapped instanceof FileDialog) {
 			((FileDialog) wrapped).setMode(FileDialog.SAVE);
-			((FileDialog) wrapped).setVisible(true);
+			wrapped.setVisible(true);
 		} else {
 			((JFileChooser) wrapped).showSaveDialog(parent);
 		}

commit d140f5cb2e7092bc1e2c592fecd1e7e7f47fc88d

PlatformFileDialog: Fix compilation failure introduced in 03a0a9238f7272f26d8650b4a12b1ae93a012fc9.

src/main/java/lovelace/tartan/gui/controls/PlatformFileDialog.java
@@ -103,7 +103,8 @@ public final class PlatformFileDialog {
 			if (filter instanceof FileFilter) {
 				((JFileChooser) wrapped).setFileFilter((FileFilter) filter);
 			} else if (filter instanceof FilenameFilter) {
-				((JFileChooser) wrapped).setFileFilter(new FilenameFilterWrapper(filter));
+				((JFileChooser) wrapped).setFileFilter(new FilenameFilterWrapper(
+					(FilenameFilter) filter));
 			}
 		}
 	}