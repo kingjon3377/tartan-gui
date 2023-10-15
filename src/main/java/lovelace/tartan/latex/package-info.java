c38aaacb6215bca59f61488c6dc403ea0fe117df
Jonathan Lovelace <kingjon3377@gmail.com>
2023-10-14 20:59:35 -0400

    Fix JavaDoc paragraphing in package-info.java files.
    When JavaDoc is generated, blank lines are ignored (as in HTML).

--- src/main/java/lovelace/tartan/latex/package-info.java
+++ src/main/java/lovelace/tartan/latex/package-info.java
@@ -1,8 +1,8 @@
 /**
  * This package contains code to convert our model objects to and from LaTeX.
- *
+ * <p>
  * Copyright (C) 2018 Jonathan Lovelace
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify it under the terms
  * of version 3 of the GNU General Public License as published by the Free Software
  * Foundation; see COPYING or
--- src/main/java/lovelace/tartan/model/package-info.java
+++ src/main/java/lovelace/tartan/model/package-info.java
@@ -1,9 +1,9 @@
 /**
  * This package contains data structures to represent a Scottish country dance Ball
  * program and its component elements.
- *
+ * <p>
  * Copyright (C) 2018 Jonathan Lovelace
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify it under the terms
  * of version 3 of the GNU General Public License as published by the Free Software
  * Foundation; see COPYING or
