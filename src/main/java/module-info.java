303fc37276732f98b213b9a7c6ab96bcd92c6636
Jonathan Lovelace <kingjon3377@gmail.com>
2023-10-14 23:33:35 -0400

    Add module descriptor.
    Apparently needed to compile, at least in IDE, with language level at its current
    version.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lovelace.tartan {
+	requires java.datatransfer;
+	requires java.desktop;
+	requires java.logging;
+	requires java.sql;
+	requires org.jetbrains.annotations;
+	requires org.xerial.sqlitejdbc;
+}
