e8b121b51f3e98e2ca61a0324471092b466a638d
Jonathan Lovelace <kingjon3377@gmail.com>
2024-10-16 07:51:04 -0400

    TartanMenu: Remove unused import.

--- src/main/java/lovelace/tartan/gui/TartanMenu.java
+++ src/main/java/lovelace/tartan/gui/TartanMenu.java
@@ -26,7 +26,6 @@ import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import lovelace.tartan.gui.controls.PlatformFileDialog;
 import lovelace.tartan.gui.model.ReorderableListModel;
-import lovelace.tartan.gui.model.ReorderableListModelImpl;
 import lovelace.tartan.latex.LaTeXReader;
 import lovelace.tartan.latex.LaTeXWriter;
 import lovelace.tartan.model.ProgramElement;
