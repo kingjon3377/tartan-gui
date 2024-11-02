b1b5f631fa742a2476c2527b27d2eec083965806
Jonathan Lovelace <kingjon3377@gmail.com>
2024-11-02 12:35:39 -0400

    DanceDatabase: Extract methods to condense constructor.

--- src/main/java/lovelace/tartan/db/DanceDatabase.java
+++ src/main/java/lovelace/tartan/db/DanceDatabase.java
@@ -37,116 +37,118 @@ public class DanceDatabase implements AutoCloseable {
 		return Collections.unmodifiableList(dances);
 	}
 
+	@FunctionalInterface
+	private interface ResultsFunction<T> {
+		T apply(ResultSet results) throws SQLException;
+	}
+
+	private <T> Map<Integer, T> resultsToMap(final String query,
+	                                         final ResultsFunction<T> reader)
+			throws SQLException {
+		try (final PreparedStatement statement = sql.prepareStatement(query);
+		     final ResultSet results = statement.executeQuery()) {
+			final Map<Integer, T> retval = new HashMap<>(results.getFetchSize());
+			while (results.next()) {
+				retval.put(results.getInt("id"), reader.apply(results));
+			}
+			return retval;
+		}
+	}
+
+	private <T> List<T> resultsToList(final String query, final ResultsFunction<T> reader)
+			throws SQLException {
+		try (final PreparedStatement statement = sql.prepareStatement(query);
+		     final ResultSet results = statement.executeQuery()) {
+			final List<T> retval = new ArrayList<>(results.getFetchSize());
+			while (results.next()) {
+				retval.add(reader.apply(results));
+			}
+			return retval;
+		}
+	}
+
 	public DanceDatabase(final @NotNull Path filename) throws SQLException {
 		final SQLiteDataSource ds = new SQLiteDataSource();
 		ds.setUrl("jdbc:sqlite:" + filename);
 		sql = ds.getConnection();
-		final Map<Integer, DanceType> typesMap;
-		try (final PreparedStatement typesStatement = sql.prepareStatement(
-				"SELECT id, name, short_name FROM dancetype");
-			 final ResultSet typesResults = typesStatement.executeQuery()) {
-			typesMap = new HashMap<>(typesResults.getFetchSize());
-			while (typesResults.next()) {
-				final int id = typesResults.getInt("id");
-				final String name = typesResults.getString("name");
-				final String abbreviation = typesResults.getString("short_name");
-				typesMap.put(id, new DanceTypeImpl(id, name, abbreviation));
-			}
-		}
-		final Map<Integer, DanceFormation> shapesMap;
-		try (final PreparedStatement shapesStatement = sql.prepareStatement(
-				"SELECT id, name, shortname FROM shape");
-			 final ResultSet shapesResults = shapesStatement.executeQuery()) {
-			shapesMap = new HashMap<>(shapesResults.getFetchSize());
-			while (shapesResults.next()) {
-				final int id = shapesResults.getInt("id");
-				final String name = shapesResults.getString("name");
-				final String abbreviation = shapesResults.getString("shortname");
-				shapesMap.put(id, new DanceFormationImpl(id, name, abbreviation));
-			}
-		}
-		final Map<Integer, DanceProgression> progressionsMap;
-		try (final PreparedStatement progressionsQuery = sql.prepareStatement(
-				"SELECT id, name FROM progression");
-			 final ResultSet progressionsResults = progressionsQuery.executeQuery()) {
-			progressionsMap = new HashMap<>(progressionsResults.getFetchSize());
-			while (progressionsResults.next()) {
-				final int id = progressionsResults.getInt("id");
-				final String name = progressionsResults.getString("name");
-				progressionsMap.put(id, new DanceProgressionImpl(id, name));
-			}
-		}
-		try (final PreparedStatement danceStatement = sql.prepareStatement(
-				"""
+		final Map<Integer, DanceType> typesMap =
+				resultsToMap("SELECT id, name, short_name FROM dancetype",
+						results -> new DanceTypeImpl(results.getInt("id"),
+								results.getString("name"),
+								results.getString("short_name")));
+		final Map<Integer, DanceFormation> shapesMap =
+				resultsToMap("SELECT id, name, shortname FROM shape",
+						results -> new DanceFormationImpl(results.getInt("id"),
+								results.getString("name"),
+								results.getString("shortname")));
+		final Map<Integer, DanceProgression> progressionsMap =
+				resultsToMap("SELECT id, name FROM progression",
+						results -> new DanceProgressionImpl(results.getInt("id"),
+								results.getString("name")));
+		dances = resultsToList("""
 						SELECT dance.id, dance.name, dance.barsperrepeat, \
 						dance.shape_id, dance.type_id, dance.couples_id, \
 						publication.name AS publicationName, dance.progression_id \
 						FROM dance, dancespublicationsmap, publication \
 						WHERE dance.id = dancespublicationsmap.dance_id AND \
-						publication.id = dancespublicationsmap.publication_id""");
-			 final ResultSet danceResults = danceStatement.executeQuery()) {
-			dances = new ArrayList<>(danceResults.getFetchSize());
-			while (danceResults.next()) {
-				final int id = danceResults.getInt("id");
-				final String name = danceResults.getString("name");
-				final int length = danceResults.getInt("barsperrepeat");
-				final DanceFormation shape;
-				final int shapeNum = danceResults.getInt("shape_id");
-				if (shapesMap.containsKey(shapeNum)) {
-					shape = shapesMap.get(shapeNum);
-				} else {
-					LOGGER.fine(() -> String.format(
-							"Shape for %s was SQL null or an unknown ID", name));
-					shape = DanceFormationImpl.UNKNOWN;
-				}
-				final DanceType type;
-				final int typeNum = danceResults.getInt("type_id");
-				if (typesMap.containsKey(typeNum)) {
-					type = typesMap.get(typeNum);
-				} else {
-					LOGGER.info(() -> String.format(
-							"Type for %s was SQL null or an unknown ID", name));
-					// TODO: Ceylon just asserted instead of providing a default
-					type = DanceTypeImpl.UNKNOWN;
-				}
-				final int couplesRaw = danceResults.getInt("couples_id");
-				final int couples;
-				// The database helpfully uses the number of couples for the ID in that
-				// table for dances that actually use couples, and then IDs in the 50s
-				// for dances with one to five individuals or 2 trios, in the 90s for
-				// other or unknown, and in the 100s for more complicated arrangements.
-				// For our purposes, any of the cases more complicated than a simple
-				// number of couples is filed under "other". This field can also be
-				// NULL (returned by getInt() as 0), in which case we also use "other".
-				if (couplesRaw > 0 && couplesRaw < 10) {
-					couples = couplesRaw;
-				} else if (couplesRaw == 0) {
-					LOGGER.fine(
-							() -> String.format("Number of couples for %s was SQL null",
-									name));
-					couples = -1;
-				} else {
-					couples = -1;
-				}
-				final String source = danceResults.getString("publicationname");
-				final int progressionNum = danceResults.getInt("progression_id");
-				final DanceProgression progression;
-				if (progressionsMap.containsKey(progressionNum)) {
-					progression = progressionsMap.get(progressionNum);
-				} else {
-					LOGGER.fine(() -> String.format(
-							"Progression for %s was SQL null or an unknown ID", name));
-					progression = DanceProgressionImpl.UNKNOWN;
-				}
-				dances.add(
-						new DanceRowImpl(id, name, length, shape, type, couples, source,
-								progression));
-			}
-		}
+						publication.id = dancespublicationsmap.publication_id""",
+				results -> parseDance(results, shapesMap, typesMap, progressionsMap));
 		cribStatement = sql.prepareStatement(
 				"""
 						SELECT text FROM dancecrib WHERE dance_id = ? \
 						ORDER BY format ASC LIMIT 1""");
+	}
+
+	private static <T> T getFromMap(final Map<Integer, T> map, final int num,
+	                                final String desc, final String context,
+	                                final T defaultValue) {
+		if (map.containsKey(num)) {
+			return map.get(num);
+		} else {
+			LOGGER.fine(() -> "%s for %s was SQL null or an unknown ID".formatted(desc,
+					context));
+			return defaultValue;
+		}
+	}
+
+	private static @NotNull DanceRowImpl parseDance(final ResultSet danceResults,
+	                                                 final Map<Integer, DanceFormation> shapesMap,
+	                                                 final Map<Integer, DanceType> typesMap,
+	                                                 final Map<Integer, DanceProgression> progressionsMap)
+			throws SQLException {
+		final int id = danceResults.getInt("id");
+		final String name = danceResults.getString("name");
+		final int length = danceResults.getInt("barsperrepeat");
+		final DanceFormation shape = getFromMap(shapesMap,
+				danceResults.getInt("shape_id"), "Shape", name,
+				DanceFormationImpl.UNKNOWN);
+		final DanceType type = getFromMap(typesMap, danceResults.getInt("type_id"),
+				"Type", name, DanceTypeImpl.UNKNOWN);
+		final int couplesRaw = danceResults.getInt("couples_id");
+		final int couples;
+		// The database helpfully uses the number of couples for the ID in that
+		// table for dances that actually use couples, and then IDs in the 50s
+		// for dances with one to five individuals or 2 trios, in the 90s for
+		// other or unknown, and in the 100s for more complicated arrangements.
+		// For our purposes, any of the cases more complicated than a simple
+		// number of couples is filed under "other". This field can also be
+		// NULL (returned by getInt() as 0), in which case we also use "other".
+		if (couplesRaw > 0 && couplesRaw < 10) {
+			couples = couplesRaw;
+		} else if (couplesRaw == 0) {
+			LOGGER.fine(
+					() -> String.format("Number of couples for %s was SQL null",
+							name));
+			couples = -1;
+		} else {
+			couples = -1;
+		}
+		final String source = danceResults.getString("publicationname");
+		final DanceProgression progression = getFromMap(progressionsMap,
+				danceResults.getInt("progression_id"), "Progression", name,
+				DanceProgressionImpl.UNKNOWN);
+		return new DanceRowImpl(id, name, length, shape, type, couples, source,
+				progression);
 	}
 
 	public @Nullable String cribText(final DanceRow dance) {
